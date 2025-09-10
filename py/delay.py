dd116d33103bf92f5fb8442be80f8b9aeaa2ac4f
tera <peteramajor@hotmail.com>
2025-09-09 21:05:50 -0400

    final

--- py/delay.py
+++ py/delay.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 # local global connection
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASS = os.getenv("DB_PASS", "")  
-DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_HOST = os.getenv("DB_HOST", "")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME", "flightdb")
 
